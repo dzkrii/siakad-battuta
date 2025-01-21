7b49bc431803feaf89e11e7fa525b9d48f8ca761
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2025-01-21 19:41:20 +0700

    update delete department

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -74,4 +74,50 @@ class DepartmentController extends Controller
             return to_route('admin.departments.index');
         }
     }
+
+    public function edit(Department $department): Response
+    {
+        return inertia('Admin/Departments/Edit', [
+            'page_settings' => [
+                'title' => 'Edit Program Studi',
+                'subtitle' => 'Edit program studi disini. Klik simpan setelah selesai.',
+                'method' => 'PUT',
+                'action' => route('admin.departments.update', $department),
+            ],
+            'department' => $department,
+            'faculties' => Faculty::query()->select(['id', 'name'])->orderBy('name')->get()->map(fn($item) => [
+                'value' => $item->id,
+                'label' => $item->name,
+            ]),
+        ]);
+    }
+
+    public function update(Department $department, DepartmentRequest $request): RedirectResponse
+    {
+        try {
+            $department->update([
+                'faculty_id' => $request->faculty_id,
+                'name' => $request->name,
+            ]);
+
+            flashMessage(MessageType::UPDATED->message('Program Studi'));
+            return to_route('admin.departments.index');
+        } catch (Throwable $e) {
+            flashMessage(MessageType::ERROR->message(error: $e->getMessage()), 'error');
+            return to_route('admin.departments.index');
+        }
+    }
+
+    public function destroy(Department $department): RedirectResponse
+    {
+        try {
+            $department->delete();
+
+            flashMessage(MessageType::DELETED->message('Program Studi'));
+            return to_route('admin.departments.index');
+        } catch (Throwable $e) {
+            flashMessage(MessageType::ERROR->message(error: $e->getMessage()), 'error');
+            return to_route('admin.departments.index');
+        }
+    }
 }
