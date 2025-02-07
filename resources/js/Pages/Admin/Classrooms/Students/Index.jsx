3a71cecd39abbfbb35c953cdf2e02eb98d0a352a
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2025-02-07 17:26:28 +0700

    change student classroom

--- resources/js/Pages/Admin/Classrooms/Students/Index.jsx
+++ resources/js/Pages/Admin/Classrooms/Students/Index.jsx
@@ -81,7 +81,7 @@ export default function Index(props) {
                         />
                     ) : (
                         <div className="grid grid-cols-4 gap-4">
-                            {Array.from({ length: 20 }).map((_, index) => {
+                            {Array.from({ length: 60 }).map((_, index) => {
                                 const student = classroomStudents[index];
                                 return student ? (
                                     <AlertAction
