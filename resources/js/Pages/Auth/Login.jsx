84b887ef04782e5f5ab470c13658dfd7b122e42e
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2025-02-12 11:03:03 +0700

    change bg login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -109,7 +109,7 @@ export default function Login({ status, canResetPassword }) {
                 </div>
             </div>
             <div className="hidden bg-muted lg:block">
-                <img src="/images/bg-login.webp" alt="login" className="h-full max-h-screen w-full object-cover" />
+                <img src="/images/gedung-baru-battuta.jpg" alt="login" className="object-fit w-full" />
             </div>
         </div>
     );
